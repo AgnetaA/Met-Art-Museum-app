81b24037d6593e4ff12e1f54aa18726808ebdc37
AgnetaA <agnetaaxelsson@hotmail.se>
2025-01-29 17:40:12 +0100

    see favourites

--- script.js
+++ script.js
@@ -1,45 +1,69 @@
- // URL tror jag: https://collectionapi.metmuseum.org/public/collection/v1/search?q=sunflowers
- //               https://collectionapi.metmuseum.org/public/collection/v1/objects
- //                                                                                     &perPage=20???
-
- // eller https://metmuseum.org/api/collection/collectionlisting?q=test ????
-
+//some variables
 const artContainerEl = document.getElementById("art-container");
 const favourites = [];
+const favouriteItems = document.getElementById("favourite-container");
 
- 
-console.log("Vad händer???");
 
 const formEl = document.getElementById("search-form");  //the whole form
 const inputEl = document.getElementById("search-input");
 
-
+renderFavouritesList();
 
 
 //submit search-form
 formEl.addEventListener("submit", (event) => {
     event.preventDefault();
     console.log("inputten: " + inputEl.value);
-
-    const query = inputEl.value; //ska bli input value
-    console.log("Query: " + query);
+    const query = inputEl.value; 
     fetchArt(query); //call function to get searchResult
 });
- 
 
 
-function addToFavouriteList (objData) {
-    console.log("knappen klickades");
+function seeTheList(favObject){
 
+    console.log(favObject.title);
 
-    favourites.push(objData);
-    console.log(favourites);
+    const favouriteArticle = document.createElement('article');
+    favouriteArticle.classList.add('art-piece');
+
+    favouriteArticle.innerHTML = `
+        <img src="${favObject.primaryImageSmall}" alt="Photo of ${favObject.title}"/>
+        <a href="${favObject.objectURL}" target="_blank"><h3>${favObject.title}</h3></a>
+    `;
+
+    favouriteItems.appendChild(favouriteArticle);
 }
 
 
-    
+//print saved favourites
+function renderFavouritesList () {
+    const favouritesList = JSON.parse(localStorage.getItem('favourite_art'));
+    console.log(favouritesList);
+       
 
+    for (let i = 0; i < favouritesList.length; i++) {
+        const favObject = favouritesList[i];
+        
+        seeTheList(favObject);
+       
+    }
+
+ 
+}
+
+
+//Add to favourites and localstorage
+function addToFavouriteList (objData) {
+    console.log("knappen klickades");
+
+    favourites.push(objData);
+    console.log(favourites);
+
+    localStorage.setItem("favourite_art", JSON.stringify(favourites));
+
+    renderFavouritesList();
    
+}
 
 
 
@@ -47,11 +71,6 @@ function addToFavouriteList (objData) {
  //function for fetching search-results from API
 async function fetchArt(query) {
     const searchResult = `https://collectionapi.metmuseum.org/public/collection/v1/search?q=${query}`;
-
-    //const searchResultTest = `https://collectionapi.metmuseum.org/public/collection/v1/objects/485308`;
-
-    //console.log(searchResultTest);
-
 
 
     try {
@@ -65,24 +84,12 @@ async function fetchArt(query) {
 
         //get IDs from API
         const resultArray = data.objectIDs;
-        //console.log(resultArray);
-        
-
-        // fetch only the 10 first ID:s
-        //sending result = ID:s to another function
+          
+        //sending result = ID:s to another function, fetch only the 10 first ID:s
         for (let i = 0; i < 10; i++) {
             const ID = resultArray[i];
             fetchArtObjects(ID);
-
         }
-
-        // //sending result = ID:s to another function
-        // resultArray.forEach(ID => {
-
-        //     fetchArtObjects(ID);
-        
-        // });
-
 
     }   catch (error) {
         console.error(error);
@@ -123,10 +130,7 @@ async function fetchArt(query) {
                 
                 <button type="submit" id="${buttonID}">Lägg till i favoriter</button>
                 `;
-
                 artContainerEl.appendChild(artArticle); 
-
-
 
                 //Add to favourites-button
                 const addFavButton = document.getElementById(buttonID);
@@ -137,18 +141,11 @@ async function fetchArt(query) {
                
                 });
             }
-
-            
-
-
-
-            
+        
         }   catch (error) {
             console.error(error);
         }
-
     };
-
 
 }
 
