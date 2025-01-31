f6fa859576503b81702bfef54c7e0df50c58f81c
AgnetaA <agnetaaxelsson@hotmail.se>
2025-01-31 13:01:55 +0100

    delete from favourites added

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ const artContainerEl = document.getElementById("art-container");
 const favouriteItemsEl = document.getElementById("favourite-container");
 const formEl = document.getElementById("search-form");  //the whole form
 const inputEl = document.getElementById("search-input");
-
+let deleteBtnClicked = false;
 
 getFavouriteObjects();
 
 //Send all objects in favourite-array (localstorage) to be rendered as the page loads
 for (let i = 0; i < favouriteObjects.length; i++) {
     const favObject = favouriteObjects[i];
-    
-    console.log(favObject.title);       
-    
+
+    console.log(favObject.title);
+
     renderObjectToDom(favouriteObjects[i]);
 }
 
@@ -22,12 +22,14 @@ for (let i = 0; i < favouriteObjects.length; i++) {
 formEl.addEventListener("submit", (event) => {
     event.preventDefault();
     console.log("inputten: " + inputEl.value);
-    const query = inputEl.value; 
+    const query = inputEl.value;
     fetchArt(query); //call function to get searchResult
 });
 
 //Write the HTML for a favourite object
-function renderObjectToDom(favObject){
+function renderObjectToDom(favObject) {
+
+    let buttonID = "removeFromFavs" + favObject.objectID;
 
     const favouriteArticle = document.createElement('article');
     favouriteArticle.classList.add('art-piece');
@@ -35,59 +37,101 @@ function renderObjectToDom(favObject){
     favouriteArticle.innerHTML = `
         <img src="${favObject.primaryImageSmall}" alt="Photo of ${favObject.title}"/>
         <a href="${favObject.objectURL}" target="_blank"><h3>${favObject.title}</h3></a>
+
+        <button type="submit" id="${buttonID}">Ta bort</button>
     `;
 
-    favouriteItemsEl.appendChild(favouriteArticle);
+    if (deleteBtnClicked === false) {
+        favouriteItemsEl.appendChild(favouriteArticle);
+    }
+
+    const removeFavButton = document.getElementById(buttonID);
+    removeFavButton.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        deleteBtnClicked = true;
+
+        deleteFromFavouriteList(favObject); //deletes from array
+
+        reWriteFavourites(favouriteObjects);
+    });
+
 }
 
+//render favourites anew after deleting one item
+function reWriteFavourites(favouriteObjects) {
+    deleteBtnClicked = false;
+
+    favouriteObjects = JSON.parse(localStorage.getItem('favourite_art') || "[]");
+    favouriteItemsEl.innerHTML = "";
+
+    for (let i = 0; i < favouriteObjects.length; i++) {
+        favObject = favouriteObjects[i];      
+        renderObjectToDom(favObject);
+    }
+}
 
 //get saved favourites
-function getFavouriteObjects () {
+function getFavouriteObjects() {
     favouriteObjects = JSON.parse(localStorage.getItem('favourite_art') || "[]");
-    
+
     console.log("favouriteObjects:", favouriteObjects)
 }
 
 
 //Add to favourites and localstorage
-function addToFavouriteList (objData) {
+function addToFavouriteList(objData) {
 
-    console.log("objData",objData);
+    console.log("objData", objData);
 
     favouriteObjects.push(objData);
 
     localStorage.setItem("favourite_art", JSON.stringify(favouriteObjects));
-    console.log("favouriteObjects",favouriteObjects);
+    console.log("favouriteObjects", favouriteObjects);
 
-    renderObjectToDom(objData);  
+    renderObjectToDom(objData);
+}
+
+//delete an object from favourites
+function deleteFromFavouriteList(favObject) {
+
+    const index = favouriteObjects.findIndex((fav) => fav.objectID === favObject.objectID);
+    console.log(index);
+
+    favouriteObjects.splice(index, 1);
+    console.log(favouriteObjects);
+
+    //update DOM
+    localStorage.setItem("favourite_art", JSON.stringify(favouriteObjects));
+
+    renderObjectToDom(favObject);
 }
 
 
- //function for fetching search-results from API
+//function for fetching search-results from API
 async function fetchArt(query) {
     const searchResult = `https://collectionapi.metmuseum.org/public/collection/v1/search?q=${query}`;
 
     try {
         const response = await fetch(searchResult);
-        if(!response.ok) {
+        if (!response.ok) {
             throw new Error(`Error: ${response.status}`);
         }
-        const data = await response.json(); 
+        const data = await response.json();
         console.log(data);
 
         //get IDs from API
         const resultArray = data.objectIDs;
-          
+
         //sending result = ID:s to another function, fetch only the 10 first ID:s
         for (let i = 0; i < 10; i++) {
             const ID = resultArray[i];
             fetchArtObjects(ID);
         }
 
-    }   catch (error) {
+    } catch (error) {
         console.error(error);
     }
-
 
     //get usable objects out of the ID:s
     async function fetchArtObjects(ID) {
@@ -98,17 +142,17 @@ async function fetchArt(query) {
 
         try {
             const objResponse = await fetch(objectSearchResult);
-            if(!objResponse.ok) {
+            if (!objResponse.ok) {
                 throw new Error(`Error: ${objResponse.status}`);
             }
             const objData = await objResponse.json();
 
             //filter out non-public domain objects (they don't have images in API)
-           if(objData.isPublicDomain === true) {
-        
+            if (objData.isPublicDomain === true) {
+
                 console.log(objData);
                 let buttonID = "addToFavs" + objData.objectID; //Use ID in object to create unique id:s for the button
-             
+
                 //render search results
                 const artArticle = document.createElement('article');
                 artArticle.classList.add('art-piece');
@@ -119,7 +163,7 @@ async function fetchArt(query) {
                 
                 <button type="submit" id="${buttonID}">Lägg till i favoriter</button>
                 `;
-                artContainerEl.appendChild(artArticle); 
+                artContainerEl.appendChild(artArticle);
 
                 //Add to favourites-button
                 const addFavButton = document.getElementById(buttonID);
@@ -127,11 +171,10 @@ async function fetchArt(query) {
                     event.preventDefault();
 
                     addToFavouriteList(objData);
-               
                 });
             }
-        
-        }   catch (error) {
+
+        } catch (error) {
             console.error(error);
         }
     };
